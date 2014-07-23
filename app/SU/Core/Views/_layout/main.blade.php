639338e811b062807c778ee51c65058f30018a73
Michael Parfyonov <slider23@gmail.com>
2014-07-23 22:31:04 +0400

    metrika

--- app/SU/Core/Views/_layout/main.blade.php
+++ app/SU/Core/Views/_layout/main.blade.php
@@ -71,5 +71,41 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="/vendor/jquery/jquery-1.11.1.min.js"></script>
 <script src="/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Yandex.Metrika informer -->
+<a href="https://metrika.yandex.ru/stat/?id=13104421&amp;from=informer"
+   target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/13104421/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                                       style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:13104421,lang:'ru'});return false}catch(e){}"/></a>
+<!-- /Yandex.Metrika informer -->
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+	(function (d, w, c) {
+		(w[c] = w[c] || []).push(function() {
+			try {
+				w.yaCounter13104421 = new Ya.Metrika({id:13104421,
+					webvisor:true,
+					clickmap:true,
+					trackLinks:true,
+					accurateTrackBounce:true,
+					ut:"noindex"});
+			} catch(e) { }
+		});
+
+		var n = d.getElementsByTagName("script")[0],
+				s = d.createElement("script"),
+				f = function () { n.parentNode.insertBefore(s, n); };
+		s.type = "text/javascript";
+		s.async = true;
+		s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+		if (w.opera == "[object Opera]") {
+			d.addEventListener("DOMContentLoaded", f, false);
+		} else { f(); }
+	})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/13104421?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
