22e4944d6237063883734a57ba991e3f8796d387
Michael Parfyonov <slider23@gmail.com>
2014-07-23 22:32:33 +0400

    metrika

--- app/SU/Core/Views/_layout/main.blade.php
+++ app/SU/Core/Views/_layout/main.blade.php
@@ -75,7 +75,7 @@
 <!-- Yandex.Metrika informer -->
 <a href="https://metrika.yandex.ru/stat/?id=13104421&amp;from=informer"
    target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/13104421/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
-                                       style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:13104421,lang:'ru'});return false}catch(e){}"/></a>
+                                       style="width:1px; height:1px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:13104421,lang:'ru'});return false}catch(e){}"/></a>
 <!-- /Yandex.Metrika informer -->
 
 <!-- Yandex.Metrika counter -->
