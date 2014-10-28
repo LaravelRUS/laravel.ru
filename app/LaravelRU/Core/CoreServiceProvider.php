7683e000c3329d27ee591f6ce8903fe91e9e9f8b
Michael Parfyonov <slider23@gmail.com>
2014-10-28 09:31:04 +0300

    remove links to Core\Views

--- app/LaravelRU/Core/CoreServiceProvider.php
+++ app/LaravelRU/Core/CoreServiceProvider.php
@@ -16,9 +16,9 @@ class CoreServiceProvider extends ServiceProvider{
 		$this->app->register('LaravelRU\Access\AccessServiceProvider');
 
 		// регистрация папки Views
-		$viewPaths = \Config::get('view.paths');
-		$viewPaths[] = __DIR__ . '/Views';
-		\Config::set('view.paths', $viewPaths);
+//		$viewPaths = \Config::get('view.paths');
+//		$viewPaths[] = __DIR__ . '/Views';
+//		\Config::set('view.paths', $viewPaths);
 
 		include __DIR__.'/core_exceptions.php';
 		//include __DIR__.'/core_routes.php';
