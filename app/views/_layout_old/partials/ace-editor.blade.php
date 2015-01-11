da57c533360eb4a4810bf9fd7135207d52b663d0
Uladzislau Shcharbin <vlad.shcherbin@gmail.com>
2015-01-12 02:29:22 +0300

    remove old layout

--- app/views/_layout_old/partials/ace-editor.blade.php
+++
@@ -1,8 +0,0 @@
-<script src="/vendor/ace/src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
-<script>
-	$(document).ready(function() {
-		var editor = ace.edit("ace-editor");
-		editor.setTheme("ace/theme/twilight");
-		editor.getSession().setMode("ace/mode/php");
-	});
-</script>
