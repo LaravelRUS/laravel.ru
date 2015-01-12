da2c48dc7161a7e29ec9987234001789913a383b
Uladzislau Shcharbin <vlad.shcherbin@gmail.com>
2015-01-12 08:20:53 +0300

    brackets changed

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
 	<title>@yield('title')</title>
 	<meta name="description" content="@yield('meta-description')">
 	<link rel="shortcut icon" href="favicon.ico">
-	<link rel="stylesheet" href="{{{ asset('css/style.min.css') }}}">
+	<link rel="stylesheet" href="{{ asset('css/style.min.css') }}">
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -20,6 +20,6 @@
 	@yield("container")
 	@include('layouts.partials.footer')
 
-	<script src="{{{ asset('js/script.min.js') }}}"></script>
+	<script src="{{ asset('js/script.min.js') }}"></script>
 </body>
 </html>
