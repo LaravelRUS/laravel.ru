3875432b9678c3f3d519dccb2620d4a523353940
Uladzislau Shcharbin <vlad.shcherbin@gmail.com>
2015-01-12 14:57:36 +0300

    remove function

--- app/views/post/view_post.blade.php
+++ app/views/post/view_post.blade.php
@@ -11,7 +11,7 @@
 <h1><?= e($post->title) ?></h1>
 
 <div class="post_credentials">
-	<span class="date"><?= $post->displayPublishedAt() ?></span> <?= $post->author->displayProfile() ?>
+	<span class="date"><?= $post->displayPublishedAt() ?></span> <a href="{{ route("user.profile", $post->author->name) }}">{{ $post->author->name }}</a>
 	<?if(allowEditPost($post)){?>
 		<span class="controls"><a class="btn btn-default btn-xs" href="<?= route("post.edit",[$post->slug]) ?>">Редактировать</a></span>
 	<?}?>
