61735f4f22fd33387efccb7e2f5d5c418474dc49
Uladzislau Shcharbin <vlad.shcherbin@gmail.com>
2015-01-12 11:04:52 +0300

    footer refactored + small scss fixes

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -1,27 +1,31 @@
-<footer>
+<footer class="bg-white">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-sx-12">
-				<div class="footer-group">
-					<h4>Присоединяйтесь</h4>
-					<ul>
-						<li><a href="https://gitter.im/LaravelRUS/chat">Чат в Gitter</a></li>
-						<li><a href="https://vk.com/laravel_rus">Сообщество Вконтакте</a></li>
-						<li><a href="https://twitter.com/LaravelRUS">Твиттер</a></li>
-					</ul>
-				</div>
-				<div class="footer-group">
-					<h4>Ресурсы</h4>
-					<ul>
-						<li><a href="http://laravel.com">Официальный сайт</a></li>
-						<li><a href="https://laravel.io">Официальный форум</a></li>
-						<li><a href="http://packalyst.com">Composer-пакеты Laravel</a></li>
-					</ul>
-				</div>
-				<div class="footer-group">
-					<h3>Знаете ли вы, что..</h3>
-				</div>
-			</div>
+			<section class="col-xs-12 col-sm-4 col-md-3">
+				<header>
+					<h4 class="h6 text-uppercase">Присоединяйтесь</h4>
+				</header>
+				<ul class="list-unstyled">
+					<li><a href="https://gitter.im/LaravelRUS/chat">Чат в Gitter</a></li>
+					<li><a href="https://vk.com/laravel_rus">Сообщество Вконтакте</a></li>
+					<li><a href="https://twitter.com/LaravelRUS">Твиттер</a></li>
+				</ul>
+			</section>
+			<section class="col-xs-12 col-sm-4 col-md-3">
+				<header>
+					<h4 class="h6 text-uppercase">Ресурсы</h4>
+				</header>
+				<ul class="list-unstyled">
+					<li><a href="http://laravel.com">Официальный сайт</a></li>
+					<li><a href="https://laravel.io">Официальный форум</a></li>
+					<li><a href="http://packalyst.com">Composer-пакеты Laravel</a></li>
+				</ul>
+			</section>
+			<section class="col-xs-12 col-sm-4 col-md-3">
+				<header>
+					<h4 class="h6 text-uppercase">Свободная колонка</h4>
+				</header>
+			</section>
 		</div>
 	</div>
 </footer>
