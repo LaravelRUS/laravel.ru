32dbbf8ba97e1e4f7b4c8fd3a5fc46fc21a3e586
vanchelo <npobolka@ya.ru>
2015-01-13 23:59:03 +0200

    Refactored GithubRepo

--- app/LaravelRU/Github/GithubRepo.php
+++ app/LaravelRU/Github/GithubRepo.php
@@ -1,76 +1,108 @@
 <?php namespace LaravelRU\Github;
 
+use Config;
+use Github\Client as GithubClient;
+use Guzzle\Http\Client as GuzzleClient;
+
 class GithubRepo {
 
 	/**
 	 * @var \Github\Client
 	 */
 	private $githubClient;
+
 	/**
 	 * @var \Guzzle\Http\Client
 	 */
-	private $http;
+	private $guzzle;
+
 	private $github_user;
+
 	private $github_repository;
 
 	public function __construct($github_user, $github_repository)
 	{
-		$this->githubClient = new \Github\Client;
-		$this->githubClient->authenticate(\Config::get("laravel.github_authenticate_token"), null, \Github\Client::AUTH_URL_TOKEN );
+		$this->githubClient = new GithubClient;
+		$this->githubClient->authenticate(
+			Config::get('laravel.github_authenticate_token'), null, GithubClient::AUTH_URL_TOKEN
+		);
 
-		$this->guzzle = new \Guzzle\Http\Client;
+		$this->guzzle = new GuzzleClient;
 		$this->github_user = $github_user;
 		$this->github_repository = $github_repository;
 	}
 
 	public function getLastCommitId($branch, $filename)
 	{
-		$response = $this->githubClient->api('repo')->commits()->all($this->github_user, $this->github_repository, array('sha' => $branch, 'path' => $filename));
-		if(count($response)!=0){
-			return $response[0]['sha'];
-		}else{
-			return null;
-		}
+		$response = $this->githubClient->api('repo')->commits()->all(
+			$this->github_user, $this->github_repository, [
+				'sha' => $branch,
+				'path' => $filename
+			]
+		);
+
+		if (count($response)) return $response[0]['sha'];
+
+		return null;
 	}
 
 	public function getLastCommit($branch, $filename)
 	{
-		$response = $this->githubClient->api('repo')->commits()->all($this->github_user, $this->github_repository, array('sha' => $branch, 'path' => $filename));
-		if(count($response)!=0){
-			return $response[0];
-		}else{
-			return null;
-		}
+		$response = $this->githubClient->api('repo')->commits()->all(
+			$this->github_user, $this->github_repository, [
+				'sha' => $branch,
+				'path' => $filename
+			]
+		);
+
+		if (count($response)) return $response[0];
+
+		return null;
 	}
 
 	public function getCommit($commit_id)
 	{
-		$response = $this->githubClient->api('repo')->commits()->show($this->github_user, $this->github_repository, $commit_id);
-		if(count($response)!=0){
-			return $response;
-		}else{
-			return null;
-		}
+		$response = $this->githubClient->api('repo')->commits()->show(
+			$this->github_user, $this->github_repository, $commit_id
+		);
+
+		if (count($response)) return $response;
+
+		return null;
 	}
 
 	public function getCommits($branch, $filename, $since)
 	{
-		$since = date("c", strtotime($since));
-		$response = $this->githubClient->api('repo')->commits()->all($this->github_user, $this->github_repository, array('sha' => $branch, 'path' => $filename, 'since'=>$since));
+		$since = date('c', strtotime($since));
+
+		$response = $this->githubClient->api('repo')->commits()->all(
+			$this->github_user, $this->github_repository, [
+				'sha' => $branch,
+				'path' => $filename,
+				'since' => $since
+			]
+		);
+
 		return $response;
 	}
 
-	public function getFile($branch, $filename, $commit_id="")
+	public function getFile($branch, $filename, $commit_id = '')
 	{
-		if( ! $commit_id){
+		if ( ! $commit_id)
+		{
 			$commit_id = $this->getLastCommitId($branch, $filename);
-			if( ! $commit_id) return null;
+
+			if ( ! $commit_id) return null;
 		}
-		$request = $this->guzzle->get("https://raw.githubusercontent.com/".
-				$this->github_user."/".
-				$this->github_repository."/$commit_id/$filename");
+
+		$request = $this->guzzle->get('https://raw.githubusercontent.com/'
+			. $this->github_user . '/'
+			. $this->github_repository
+			. "/$commit_id/$filename"
+		);
 		$response = $request->send();
+
 		return $response->getBody(true);
 	}
 
-} 
+}
