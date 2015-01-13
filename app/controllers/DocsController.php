dcfef8307f5a6f242874b6019e2135a0794068e8
vanchelo <npobolka@ya.ru>
2015-01-13 19:43:10 +0200

    Update doc controller

--- app/controllers/DocsController.php
+++ app/controllers/DocsController.php
@@ -1,63 +1,104 @@
 <?php
 
-use LaravelRU\Docs\DocsUtil;
+//use LaravelRU\Docs\DocsUtil;
 
 // TODO change config storage to DB
 // TODO если такая страница, но другой версии отсутствует - кидает 404. изменить
 // TODO change to compact()
+use Illuminate\Database\Eloquent\Collection;
+
 class DocsController extends BaseController {
 
+	/**
+	 * @var Collection|static[]
+	 */
 	protected $versions;
 
+	/**
+	 * @var Version
+	 */
 	protected $default_version;
+
+	/**
+	 * @var Version
+	 */
+	protected $master_version;
 
 	public function __construct()
 	{
-		//Todo: закешировать?
-		$this->versions = Version::lists('is_default', 'iteration');
+		// TODO закешировать?
+		/** @var Collection versions */
+		$this->versions = Version::all();
 
-		$this->default_version = array_search(1, $this->versions);
+		$this->default_version = $this->versions->first(function ($key, $item)
+		{
+			return $item->is_default;
+		});
+
+		$this->master_version = $this->versions->first(function ($key, $item)
+		{
+			return $item->is_master;
+		});
 	}
 
 	public function defaultDocs($name = 'installation')
 	{
-		return Redirect::route('docs', [$this->default_version, $name]);
+		return Redirect::route('docs', [$this->default_version->iteration, $name]);
 	}
 
-	public function docs($version = '', $name = 'installation')
+	public function docs($iteration = '', $name = 'installation')
 	{
-		if ( ! isset($this->versions[$version]))
+		if ($iteration == 'master')
 		{
-			// Запрошен универсальный урл типа /docs/installation - средиректить на /docs/5.0/installation
+			$version = $this->master_version;
+		}
+		else
+		{
+			$version = $this->versions->first(function ($key, $item) use ($iteration)
+			{
+				return $item->iteration == $iteration;
+			});
+		}
+
+		if ( ! $version)
+		{
+			/**
+			 * Запрошен универсальный урл типа /docs/installation
+			 * средиректить на /docs/4.2/installation
+			 */
 			$session_version = Session::get('docs_version');
 
 			if ( ! $session_version)
 			{
-				$session_version = $this->default_version;
+				$session_version = $this->default_version->iteration;
 			}
 
-			return Redirect::route('docs', [$session_version, $version]);
+			return Redirect::route('docs', [$session_version, $name]);
 		}
 
-		//Todo: ловим одно исключение чтобы выбросить другое?
 		$page = Document::version($version)->name($name)->first();
-		if ( ! $page) abort();
 
-		Session::set('docs_version', $version);
+		if ( ! $page)
+		{
+			return Redirect::route('docs', [$this->default_version->iteration, $name]);
+		}
+
+		Session::set('docs_version', $iteration);
+
 		$menu = Document::version($version)->name('documentation')->first();
 
 		return View::make('docs.docs-page', compact('page', 'menu', 'version', 'name'));
 	}
 
-	// список страниц с
+	// TODO Что вообще делает этот метод?
 	public function updates()
 	{
 		$docs = [];
 
 		// Todo: запросы в цикле? Выпилить.
-		foreach ($this->versions as $version => $is_default)
+		foreach ($this->versions as $version)
 		{
-			$docs[$version] = Document::version($version)->orderBy('name', 'ASC')->get();
+			$docs[$version->iteration] = Document::version($version)->orderBy('name', 'ASC')->get();
 		}
 
 		return View::make('docs/updates', compact('docs'));
