9f28a77be17aa23de33e97c002092370972b7894
vanchelo <npobolka@ya.ru>
2015-01-13 21:20:26 +0200

    Some small changes on configs

--- app/config/laravel.php
+++ app/config/laravel.php
@@ -1,26 +1,26 @@
 <?php
 
-return array(
+return [
 
 	'versions' => [
-		"master",
-		"4.2",
-		"4.1",
+		'master',
+		'4.2',
+		'4.1',
 	],
 
-	'default_version' => "4.2",
+	'default_version' => '4.2',
 
-	'original_docs' =>[
-		'user'=>"laravel",
-		'repository'=>"docs"
+	'original_docs' => [
+		'user' => 'laravel',
+		'repository' => 'docs'
 	],
 
 	'translated_docs' => [
-		'user'=>"LaravelRUS",
-		'repository'=>"docs"
+		'user' => 'LaravelRUS',
+		'repository' => 'docs'
 	],
 
 
-	'github_authenticate_token' => "3b738c9da9a24167bf6fb750a86ddfb64edbc1db"
+	'github_authenticate_token' => '3b738c9da9a24167bf6fb750a86ddfb64edbc1db'
 
-);
+];
