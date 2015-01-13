0c911c0073890a16740c8dc3c31475a5db27e3c7
vanchelo <npobolka@ya.ru>
2015-01-14 01:08:12 +0200

    Update blog/partials/short_blog_post.blade.php

--- app/views/blog/partials/short_blog_post.blade.php
+++ app/views/blog/partials/short_blog_post.blade.php
@@ -1,9 +1,10 @@
 <div class="post_short">
-	<a href="<?= route("post.view", [$post->slug]) ?>" class="post_short_link">
-		<?= e($post->title) ?>
-	</a><br>
-	<span class="text-muted">Автор: <?= $user->displayProfileUrl() ?>. </span>
+	<a href="{{ route('post.view', $post->slug) }}" class="post_short_link">
+		{{{ $post->title }}}
+	</a>
+	<br>
+	<span class="text-muted">Автор: {{ $user->displayProfileUrl() }}. </span>
 	<div class="post_short_description">
-		<?= e($post->description) ?>
+		{{{ $post->description }}}
 	</div>
 </div>
