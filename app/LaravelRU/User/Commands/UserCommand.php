96fce2af959c04c78a8d45037f0c3518c5d22581
vanchelo <npobolka@ya.ru>
2015-01-13 16:46:57 +0200

    artisan create user command

--- app/LaravelRU/User/Commands/UserCommand.php
+++ app/LaravelRU/User/Commands/UserCommand.php
@@ -1,10 +1,10 @@
 <?php namespace LaravelRU\User\Commands;
 
-use Illuminate\Console\Command;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
-use User;
 use Role;
+use User;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class UserCommand extends Command {
 
@@ -23,38 +23,34 @@ class UserCommand extends Command {
 	protected $description = 'Creating user.';
 
 	/**
-	 * Create a new command instance.
-	 *
-	 * @return UserCommand
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-	/**
 	 * Execute the console command.
 	 *
 	 * @return mixed
 	 */
 	public function fire()
 	{
-		$name = $this->argument("username");
-		$password = $this->argument("password");
-		$email = $this->argument("email");
-		$isAdmin = $this->option("admin");
+		$name = $this->argument('username');
+		$password = $this->argument('password');
+		$email = $this->argument('email');
+		$isAdmin = $this->option('admin');
 
 		$roleName = $isAdmin ? 'administrator' : 'user';
 
+		/** @var Role $role */
 		$role = Role::whereName($roleName)->first();
 
-		$user = User::create(['name'=>$name, 'password'=>$password, 'email'=>$email]);
+		/** @var User $user */
+		$user = User::create([
+			'name' => $name,
+			'password' => $password,
+			'email' => $email
+		]);
 
 		$user->is_confirmed = 1;
 
 		$role->users()->save($user);
 
-		$this->info("User $name created.");
+		$this->info("User \"$name\" created.");
 
 	}
 
@@ -65,11 +61,11 @@ class UserCommand extends Command {
 	 */
 	protected function getArguments()
 	{
-		return array(
-				array('username', InputArgument::REQUIRED, 'User Name.'),
-				array('password', InputArgument::REQUIRED, 'User Password.'),
-				array('email', InputArgument::REQUIRED, 'User Email.'),
-		);
+		return [
+			['username', InputArgument::REQUIRED, 'User Name.'],
+			['password', InputArgument::REQUIRED, 'User Password.'],
+			['email', InputArgument::REQUIRED, 'User Email.'],
+		];
 	}
 
 	/**
@@ -79,9 +75,9 @@ class UserCommand extends Command {
 	 */
 	protected function getOptions()
 	{
-		return array(
-				array('admin', 'a', InputOption::VALUE_NONE, 'Set admin permissions', null),
-		);
+		return [
+			['admin', 'a', InputOption::VALUE_NONE, 'Set admin permissions', null],
+		];
 	}
 
 }
