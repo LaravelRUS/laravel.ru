7d44b537f3733f21ec712061b59f2de33157284c
vanchelo <npobolka@ya.ru>
2015-01-13 18:46:57 +0200

    Refactoring Parser

--- app/LaravelRU/Parser/Parse.php
+++ app/LaravelRU/Parser/Parse.php
@@ -4,27 +4,26 @@ use Parsedown;
 
 class Parse {
 
-    private $markdownParser;
+	private $markdownParser;
 
-    public function __construct()
-    {
-        $this->markdownParser = new Parsedown();
-    }
+	public function __construct()
+	{
+		$this->markdownParser = new Parsedown();
+	}
 
-    public function urlToAhref($text)
-    {
-        $html = preg_replace("#(http://.*?)(\s|,|;|\:|\)|\]|\>|\}|$)#", "<a href=\"\\1\" target=\"_blank\">\\1</a>\\2", $text);
-        return $html;
-    }
+	public function urlToAhref($text)
+	{
+		$html = preg_replace('#(http://.*?)(\s|,|;|\:|\)|\]|\>|\}|$)#', "<a href=\"\\1\" target=\"_blank\">\\1</a>\\2", $text);
 
+		return $html;
+	}
 
-    public function markdown($text)
-    {
-        $text = strip_tags($text);
-        $html = $this->markdownParser->text($text);
-        //$html = $this->urlToAhref($html);
-        return $html;
-    }
+	public function markdown($text)
+	{
+		$text = strip_tags($text);
+		$html = $this->markdownParser->text($text);
 
+		return $html;
+	}
 
-} 
+}
