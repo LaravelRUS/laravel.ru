15d76e087e35faec7195d3963c6c94e38c76c484
Роман Сохарев <greabock@gmail.com>
2015-01-15 00:33:46 +0700

    Adding "LikeableTrait" to likeable Models

--- app/models/Package.php
+++ app/models/Package.php
@@ -1,10 +1,13 @@
 <?php
 
+use LaravelRU\Likes\LikeableTrait;
 use LaravelRU\Comment\CommentableTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Package extends Model {
 
 	use CommentableTrait;
+
+	use LikeableTrait;
 
 }
