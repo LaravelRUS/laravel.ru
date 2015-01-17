08c2f1dcdcb64db67e7884fc8533efe86f68f4e1
Michael Parfyonov <slider23@gmail.com>
2015-01-17 17:24:15 +0300

    add mdash

--- app/views/home/home-page.blade.php
+++ app/views/home/home-page.blade.php
@@ -6,7 +6,7 @@
 @section('container')
 	<div class="homepage hexagons text-center">
 		<div class="container">
-			<h1 class="c-white letter-spacing-stiff">Laravel - php-фреймворк нового поколения</h1>
+			<h1 class="c-white letter-spacing-stiff">Laravel &mdash; php-фреймворк нового поколения</h1>
 			<h2 class="c-white small m-b-25">Мы верим, что процесс разработки только тогда наиболее продуктивен, когда работа с фреймворком приносит радость и удовольствие. Счастливые разработчики пишут лучший код.</h2>
 			<ul class="list-inline">
 				<li>
