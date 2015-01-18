8c06622e4c73effd5af7d180aef1559d14170e67
vanchelo <brezhnev.ivan@yahoo.com>
2015-01-18 04:06:03 +0200

    footer info block

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -23,6 +23,15 @@
 			</section>
 			<section class="col-xs-12 col-sm-4 col-md-3">
 				<header>
+					<h4 class="h6 text-uppercase">Инфо</h4>
+				</header>
+				<ul class="list-unstyled">
+					<li><a href="/help/rules">Правила</a></li>
+					<li><a href="/help/agreement">Соглашение</a></li>
+				</ul>
+			</section>
+			<section class="col-xs-12 col-sm-4 col-md-3">
+				<header>
 					<h4 class="h6 text-uppercase">Свободная колонка</h4>
 				</header>
 			</section>
