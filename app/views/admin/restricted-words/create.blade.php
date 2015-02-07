30ef81409a6202df7113376de26af27b63f78fc0
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2015-02-08 00:03:28 +0300

    restricted words views

---
+++ app/views/admin/restricted-words/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+
+@section('title', 'Новое слово')
+
+@section('container')
+	<div class="col-xs-12">
+		<main>
+			<article class="bg-white p-35-45">
+				<header>
+					<h1 class="h2">Новое слово</h1>
+				</header>
+				<hr>
+
+			</article>
+		</main>
+	</div>
+@stop
