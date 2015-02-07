5625131423a7a1028076dc6da1e04fa9e554ada8
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2015-02-08 00:50:13 +0300

    quick fix

--- app/views/admin/restricted-words/show.blade.php
+++ app/views/admin/restricted-words/show.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.admin')
 
-@section('title', $word->title)
+@section('title', $restrictedWord->title)
 
 @section('container')
 	<div class="col-xs-12">
 		<main>
 			<article class="bg-white p-35-45">
 				<header>
-					<h1 class="h2">$word->title</h1>
+					<h1 class="h2">{{ $restrictedWord->title }}</h1>
 				</header>
 				<hr>
 
