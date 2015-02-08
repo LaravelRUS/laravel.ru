dd6d57823234e8ec4c89d8b97a184f3f9e6b739b
vanchelo <brezhnev.ivan@yahoo.com>
2015-02-09 01:27:14 +0200

    Fixed word deletion

--- resources/js/data-table.init.js
+++ resources/js/data-table.init.js
@@ -6,20 +6,20 @@ $(document).ready(function () {
 				url: window.location.href
 			},
 			columns: [{
-					data: 'id'
-				},{
-					data: 'title'
-				},{
-					data: 'created_at'
-				},{
-					data: 'updated_at'
-				},{
-					data: 'id',
-					render: function (id) {
-						return '<a href="#" onclick="Admin.editEntity('+id+');return false">Edit</a>' +
-							'<a href="#" onclick="Admin.destroyEntity('+id+');return false">Delete</a>';
-					}
+				data: 'id'
+			}, {
+				data: 'title'
+			}, {
+				data: 'created_at'
+			}, {
+				data: 'updated_at'
+			}, {
+				data: 'id',
+				render: function (id) {
+					return '<a href="#" onclick="Admin.editEntity(' + id + ');return false">Edit</a>' +
+						'<a href="#" onclick="Admin.destroyEntity(' + id + ');return false">Delete</a>';
 				}
+			}
 			]
 		});
 	}
@@ -29,10 +29,32 @@ Admin.editEntity = function (id) {
 	window.location.pathname += '/' + id + '/edit';
 };
 Admin.destroyEntity = function (id) {
-	if (!confirm('Delete?')) return false;
-
-	$.ajax({
-		url: window.location.pathname + '/' + id,
-		type: 'delete'
+	swal({
+		title: 'Are you sure?',
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#DD6B55',
+		confirmButtonText: 'Yes, delete it!',
+		closeOnConfirm: false
+	}, function () {
+		$.ajax({
+			url: window.location.pathname + '/' + id,
+			type: 'delete',
+			success: function (data) {
+				if (data.success) {
+					sweetAlert({
+						title: data.message,
+						type: 'success'
+					}, function () {
+						$('.data-table').DataTable().ajax.reload()
+					});
+				} else if (data.error) {
+					sweetAlert({
+						title: data.message,
+						type: 'error'
+					});
+				}
+			}
+		});
 	});
 };
