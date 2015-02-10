9b22cb2acd37a46df3a5e5dfa2b2b35c371bc80b
vanchelo <brezhnev.ivan@yahoo.com>
2015-02-10 02:59:31 +0200

    Add avatar_path helper function

--- app/LaravelRU/User/user_helpers.php
+++ app/LaravelRU/User/user_helpers.php
@@ -12,3 +12,11 @@ HTML::macro('social', function ($key, $user)
 		return $info;
 	}
 });
+
+if ( ! function_exists('avatar_path'))
+{
+	function avatar_path($image = '')
+	{
+		return Config::get('users.avatar_base_url') . $image;
+	}
+}
