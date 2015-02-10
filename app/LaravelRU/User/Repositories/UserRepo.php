173edd452e84320cdea1727cc0d4c3e175abbcc1
vanchelo <brezhnev.ivan@yahoo.com>
2015-02-10 22:01:10 +0200

    Add search method for UserRepo

--- app/LaravelRU/User/Repositories/UserRepo.php
+++ app/LaravelRU/User/Repositories/UserRepo.php
@@ -1,5 +1,6 @@
 <?php namespace LaravelRU\User\Repositories;
 
+use Closure;
 use LaravelRU\Core\Repository\AbstractRepository;
 use LaravelRU\User\Models\User;
 
@@ -8,6 +9,21 @@ class UserRepo extends AbstractRepository {
 	public function __construct(User $user)
 	{
 		$this->model = $user;
+	}
+
+	public function all()
+	{
+		return $this->model->withInfo()->get();
+	}
+
+	public function search($query, Closure $callback = null)
+	{
+		$q = $this->model->query();
+		$q->search($query);
+
+		if ( ! is_null($callback)) call_user_func($callback, $q);
+
+		return $q->paginate();
 	}
 
 }
