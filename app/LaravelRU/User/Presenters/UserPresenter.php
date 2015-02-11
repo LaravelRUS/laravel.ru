dac509f2688a7f18f491a5f20dcfecba322cfdec
vanchelo <brezhnev.ivan@yahoo.com>
2015-02-11 13:06:37 +0200

    Add localized dates to user presenter

--- app/LaravelRU/User/Presenters/UserPresenter.php
+++ app/LaravelRU/User/Presenters/UserPresenter.php
@@ -5,6 +5,8 @@ use LocalizedCarbon;
 
 class UserPresenter extends Presenter {
 
+	protected static $dates = ['last_activity_at', 'last_login_at', 'created_at'];
+
 	public function fullname()
 	{
 		if ( ! $this->info->name && ! $this->info->surname) return $this->entity->username;
@@ -17,9 +19,36 @@ class UserPresenter extends Presenter {
 		return LocalizedCarbon::createFromFormat('Y-m-d', $this->info->birthday)->formatLocalized('%e %f %Y');
 	}
 
-	public function created_at()
+	/**
+	 * @param \Carbon\Carbon $date
+	 * @return mixed|string
+	 */
+	private function localizedDate($date)
 	{
-		return LocalizedCarbon::instance($this->entity->created_at)->formatLocalized('%e %f %Y в %H:%M');
+		if ( ! $date || $date->year < 0) return '&mdash;';
+
+		return LocalizedCarbon::instance($date)->formatLocalized('%e %f %Y в %H:%M');
+	}
+
+	/**
+	 * Allow for property-style retrieval
+	 *
+	 * @param $property
+	 * @return mixed
+	 */
+	public function __get($property)
+	{
+		if (method_exists($this, $property))
+		{
+			return $this->{$property}();
+		}
+
+		if (in_array($property, self::$dates))
+		{
+			return $this->localizedDate($this->entity->{$property});
+		}
+
+		return $this->entity->{$property};
 	}
 
 }
