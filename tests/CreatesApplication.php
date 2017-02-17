7b0233e13c2f6d7f0f0216b8078d5c9b959a6a39
jhaoda <jhaoda@gmail.com>
2017-02-18 03:26:52 +0700

    Фикс тестов

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -12,11 +12,10 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Foundation\Application;
 
 trait CreatesApplication
 {
-    public function createApplication(): Application
+    public function createApplication()
     {
         $app = require __DIR__ . '/../bootstrap/app.php';
 
