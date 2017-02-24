81b22b2a667bfe87f23d9cfcd9f04a434dfab815
Serafim <nesk@xakep.ru>
2017-02-24 04:36:27 +0300

    Add docs into ide helper command

--- app/Console/Commands/IdeHelperRun.php
+++ app/Console/Commands/IdeHelperRun.php
@@ -15,25 +15,38 @@ use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 
 /**
  * Class IdeHelperRun.
+ *
+ * Класс запсукает все команды ide-helper пакета.
+ * Просто для хелпер, чтобы не париться и упростить генерацию.
  */
 class IdeHelperRun extends Command
 {
     /**
+     * Основное приложение. Оно есть в базовой команде, но там не работает автокомплит.
+     * по-этому переопределим его тут. Мешать не будет, а автокомплит появится.
+     *
      * @var \Illuminate\Foundation\Application
      */
     protected $laravel;
 
     /**
+     * Название нашей консольной команды.
+     *
      * @var string
      */
     protected $signature = 'ide-helper:run';
 
     /**
+     * Краткое описание нашей консольной команды.
+     *
      * @var string
      */
     protected $description = 'Generate a IDE Helper files if current environment is local.';
 
     /**
+     * Метод выполняет саму команду, которая генерирует хелперы для IDE.
+     * Помимо этого добавим проверку на окружение. На боевом сервере эти хелперы просто не нужны.
+     *
      * @return void
      */
     public function handle(): void
