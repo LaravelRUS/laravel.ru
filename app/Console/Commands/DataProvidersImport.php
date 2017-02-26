f27ad17007a8e8fdcf6a9c9a0237cae1249ddb33
Serafim <nesk@xakep.ru>
2017-02-26 08:27:16 +0300

    Add multiple bots support

--- app/Console/Commands/DataProvidersImport.php
+++ app/Console/Commands/DataProvidersImport.php
@@ -2,7 +2,6 @@
 
 /**
  * This file is part of laravel.su package.
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -10,13 +9,14 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use Carbon\Carbon;
-use App\Models\Bot;
 use App\Models\Article;
-use Illuminate\Console\Command;
-use App\Services\DataProviders\Manager;
-use App\Services\DataProviders\ExternalArticle;
+use App\Models\Bot;
 use App\Services\DataProviders\DataProviderInterface;
+use App\Services\DataProviders\ExternalArticle;
+use App\Services\DataProviders\Manager;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 /**
  * Class DataProvidersImport.
@@ -25,14 +25,12 @@ class DataProvidersImport extends Command
 {
     /**
      * The name and signature of the console command.
-     *
      * @var string
      */
     protected $signature = 'data-providers:import';
 
     /**
      * The console command description.
-     *
      * @var string
      */
     protected $description = 'Run import articles from external services';
@@ -43,35 +41,21 @@ class DataProvidersImport extends Command
     private $published = [];
 
     /**
-     * @param  Manager                   $manager
+     * @param  Manager $manager
      * @throws \InvalidArgumentException
      */
     public function handle(Manager $manager)
     {
-        $a = Article::publishedByBot()
-            ->with('user')
-            ->latest('published_at')
-            ->first();
-
-        $articles = (array) ($a ?? []);
-
-        /** @var Article $article */
-        foreach ($articles as $article) {
-            /** @var Bot $bot */
-            $bot = $article->user;
-
-            $this->import($article->published_at, $manager->get($bot->provider));
-
-            $this->published[] = $bot->provider;
-        }
+        //$this->importPublished($manager);
 
         $this->importNotPublished($manager);
     }
 
     /**
      * @param Manager $manager
+     * @return void
      */
-    private function importNotPublished(Manager $manager)
+    private function importNotPublished(Manager $manager): void
     {
         foreach ($manager as $alias => $provider) {
             if (in_array($alias, $this->published, true)) {
@@ -82,6 +66,10 @@ class DataProvidersImport extends Command
         }
     }
 
+    /**
+     * @param \DateTime             $time
+     * @param DataProviderInterface $provider
+     */
     private function import(\DateTime $time, DataProviderInterface $provider)
     {
         /** @var ExternalArticle[] $latest */
@@ -96,6 +84,44 @@ class DataProvidersImport extends Command
             $external->fill($article);
 
             $article->save();
+        }
+    }
+
+    /**
+     * @param Manager $manager
+     * @throws \InvalidArgumentException
+     * @return void
+     */
+    private function importPublished(Manager $manager): void
+    {
+        /** @var Article $article */
+        foreach ($this->getLatestBotArticles() as $article) {
+            /** @var Bot $bot */
+            $bot = $article->user;
+
+            $this->import($article->published_at, $manager->get($bot->provider));
+
+            $this->published[] = $bot->provider;
+        }
+    }
+
+    /**
+     * @return \Generator|Article[]
+     */
+    private function getLatestBotArticles(): \Generator
+    {
+        $bots = Bot::query()
+            ->with(['articles' => function (MorphMany $relation) {
+                return $relation->latest('published_at')->take(1);
+            }])
+            ->get();
+
+        foreach ($bots as $bot) {
+            $article = $bot->articles->first();
+
+            if ($article) {
+                yield $article;
+            }
         }
     }
 }
