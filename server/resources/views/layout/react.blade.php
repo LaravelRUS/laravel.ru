d8798eae7044ccda7ec5d90931dfab3d6ed6b926
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2017-03-12 13:02:40 +0300

    Update react view

--- server/resources/views/layout/react.blade.php
+++ server/resources/views/layout/react.blade.php
@@ -2,14 +2,15 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>{{ config('app.name') }}</title>
     <meta name="token" content="{!! $token !!}" />
 </head>
 <body>
-    <main id="app"></main>
-    <script src="{{ asset_ts('app.js') }}"></script>
+    <div id="app"></div>
+    <script src="{{ asset_ts('bundle.js') }}"></script>
 </body>
 </html>
