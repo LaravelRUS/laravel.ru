734c78a9dd2c7cc086518e3d350919c6fa6dd9c9
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2017-04-06 22:29:20 +0300

    Update index file

--- client/app/index.js
+++ client/app/index.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { Provider } from 'react-redux'
-import { ApolloProvider } from 'react-apollo'
-import store from './config/store'
-import apolloClient from './config/apollo'
 import App from './App'
-import './app.scss'
+import './styles/app.scss'
 
 ReactDOM.render(
-  <ApolloProvider store={store} client={apolloClient}>
-    <App />
-  </ApolloProvider>,
+  <App />,
   document.getElementById('app')
 )
