e1a5112f72e211185950a3a9ac6e99631e8565f7
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2017-04-06 22:42:51 +0300

    Update react server files

--- server/resources/views/layout/react.blade.php
+++ server/resources/views/layout/react.blade.php
@@ -8,9 +8,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>{{ config('app.name') }}</title>
     <meta name="token" content="{!! $token !!}" />
+    <link rel="stylesheet" href="{{ asset_ts('css/react.css', '') }}" />
 </head>
 <body>
     <div id="app"></div>
-    <script src="{{ asset_ts('bundle.js') }}"></script>
+    <script src="{{ asset_ts('js/react.js', '') }}"></script>
 </body>
 </html>
