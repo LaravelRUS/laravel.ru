f3573b114d4b98981eafbeba5581cfecd2714981
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2017-06-15 06:41:35 +0300

    Update latest component changes

--- client/app/scenes/Layout/components/Header.js
+++ client/app/scenes/Layout/components/Header.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import classNames from 'classnames'
-import { LaravelIcon, MenuIcon } from 'components/Icons'
+import { LaravelIcon } from 'components/Icons'
 
 class Header extends Component {
-  state = {
-    isMenuOpened: false
-  }
-
-  handleMenuClick = () => {
-    this.setState({ isMenuOpened: !this.state.isMenuOpened })
-  }
-
   render() {
-    const navigationClassName = classNames('navigation', {
-      opened: this.state.isMenuOpened
-    })
-
     return (
       <header className="laravel-header">
-        <div className="contents">
+        <nav className="contents">
           <div className="logo">
             <NavLink to="/" exact>
               <LaravelIcon />
@@ -29,26 +16,21 @@ class Header extends Component {
               </div>
             </NavLink>
           </div>
-          <div className="menu">
-            <button onClick={this.handleMenuClick}>
-              <MenuIcon />
-            </button>
-          </div>
-          <div className={navigationClassName}>
-            <ul>
-              <li><NavLink to="/docs">Документация</NavLink></li>
-              <li><NavLink to="/articles">Статьи</NavLink></li>
-              <li><NavLink to="/jobs">Работа</NavLink></li>
-              <li><NavLink to="/karma">Карма</NavLink></li>
-              <li><NavLink to="/packages">Пакеты</NavLink></li>
-              {/* <li><NavLink to="#">Вконтакте</NavLink></li> */}
-              <li><NavLink to="#">Чат</NavLink></li>
-              {/* <li><NavLink to="/search">Поиск</NavLink></li> */}
-              {/* <li><NavLink to="#">Tips</NavLink></li> */}
-              {/* <li><NavLink to="/login">Login</NavLink></li> */}
-            </ul>
-          </div>
-        </div>
+          <ul className="top-navigation">
+            <li><NavLink to="/search">Поиск</NavLink></li>
+            <li><NavLink to="/login">Вход</NavLink></li>
+          </ul>
+          <ul className="bottom-navigation">
+            <li><NavLink to="/docs">Документация</NavLink></li>
+            <li><NavLink to="/articles">Статьи</NavLink></li>
+            <li><NavLink to="/jobs">Работа</NavLink></li>
+            {/* <li><NavLink to="/karma">Карма</NavLink></li> */}
+            {/* <li><NavLink to="/packages">Пакеты</NavLink></li> */}
+            {/* <li><NavLink to="#">Вконтакте</NavLink></li> */}
+            {/* <li><NavLink to="#">Чат</NavLink></li> */}
+            {/* <li><NavLink to="#">Tips</NavLink></li> */}
+          </ul>
+        </nav>
       </header>
     )
   }
